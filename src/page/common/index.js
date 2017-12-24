008d2557a6e4095efca96500cacea8af2fc7061d
Jecyu <jecyu.lin@gmail.com>
2017-12-24 19:05:39 +0800

    common header and footer

---
+++ src/page/common/index.js
@@ -0,0 +1,10 @@
+// import { request } from 'https';
+
+
+'use strict';
+
+// require('bootstrap/dist/css/bootstrap.min.css');
+require('font-awesome/scss/font-awesome.scss');
+require('./nav/index.js');
+require('./layout.scss');
+require('./footer/index.scss');
---
+++ src/page/common/nav/index.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: jecyu 
+ * @Date: 2017-12-24 00:14:45 
+ * @Last Modified by: jecyu
+ * @Last Modified time: 2017-12-24 11:36:17
+ */
+
+"use strict";
+
+require("./index.scss");
+
+// 导航
+var nav = {
+    init: function() {
+        this.bindEvent();
+        return this;
+    },
+    bindEvent: function() {}
+};
+
+module.exports = nav.init();
